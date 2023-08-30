31d9bf97ee11d5822e75bc21b01197733b04f0f2
Kyle <thatkyle@gmail.com>
2023-08-29 18:40:50 -0600

    first commit

---
+++ game.py
@@ -0,0 +1,61 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Game constants
+WIDTH, HEIGHT = 640, 480
+BALL_SPEED = 5
+PADDLE_SPEED = 5
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Create the game window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong Game")
+
+# Initialize paddles and ball
+player_paddle = pygame.Rect(50, HEIGHT // 2 - 50, 10, 100)
+opponent_paddle = pygame.Rect(WIDTH - 60, HEIGHT // 2 - 50, 10, 100)
+ball = pygame.Rect(WIDTH // 2 - 15, HEIGHT // 2 - 15, 30, 30)
+ball_speed = [random.choice((-1, 1)) * BALL_SPEED, random.choice((-1, 1)) * BALL_SPEED]
+
+# Game loop
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_paddle.top > 0:
+        player_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and player_paddle.bottom < HEIGHT:
+        player_paddle.y += PADDLE_SPEED
+
+    # Ball movement and collision with walls
+    ball.x += ball_speed[0]
+    ball.y += ball_speed[1]
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed[1] = -ball_speed[1]
+
+    # Ball collision with paddles
+    if ball.colliderect(player_paddle) or ball.colliderect(opponent_paddle):
+        ball_speed[0] = -ball_speed[0]
+
+    # Update screen
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, player_paddle)
+    pygame.draw.rect(screen, WHITE, opponent_paddle)
+    pygame.draw.ellipse(screen, WHITE, ball)
+    pygame.draw.aaline(screen, WHITE, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT))
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
